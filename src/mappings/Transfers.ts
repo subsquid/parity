2ff57bf5deab0607eddb6a280ffdf9880197644a
NagatoPeinI1 <shalabhnegi123@gmail.com>
2021-12-09 17:24:05 +0530

    changed account balance logic

--- src/mappings/Transfers.ts
+++ src/mappings/Transfers.ts
@@ -80,47 +80,48 @@ const handleAccountAndBalance = async (
   let balanceValue: bigint = balance?.freeBalance || 0n;
   balanceValue = isFrom ? balanceValue - value - tip : balanceValue + value;
 
+  let account = await store.get(Account, {
+    where: { id: who },
+  });
+
+  // token data check and creation
+  let tokenData: Token | undefined = await store.get(Token, {
+    where: { id: TOKEN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
+  });
+
+  if (!tokenData) {
+    tokenData = new Token(TOKEN_DETAILS);
+    await store.save(tokenData);
+  }
+
+  // chain data check and creation
+  // const chainID = `${block.height}-${who}` || CHAIN_DETAILS.id;
+  let chainData: Chain | undefined = await store.get(Chain, {
+    // where: { id: chainID }, // This is temporary until we got way to get the chain id and token id
+    where: { id: CHAIN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
+  });
+
+  if (!chainData) {
+    // CHAIN_DETAILS.id = chainID;
+    CHAIN_DETAILS.id = CHAIN_DETAILS.id;
+    chainData = new Chain(CHAIN_DETAILS);
+    await store.save(chainData);
+  }
+
+  // account data check and creation
+  if (!account) {
+    account = new Account({
+      id: who,
+      chainId: CHAIN_DETAILS.id,
+      balance: balanceValue,
+    });
+  } else {
+    account.balance = isFrom ? (account.balance - (value + tip)) : (account.balance + value);
+  }
+
+  await store.save(account);
+
   if (!balance) {
-    let account = await store.get(Account, {
-      where: { id: who },
-    });
-
-    // token data check and creation
-    let tokenData: Token | undefined = await store.get(Token, {
-      where: { id: TOKEN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
-    });
-
-    if (!tokenData) {
-      tokenData = new Token(TOKEN_DETAILS);
-      await store.save(tokenData);
-    }
-
-    // chain data check and creation
-    // const chainID = `${block.height}-${who}` || CHAIN_DETAILS.id;
-    let chainData: Chain | undefined = await store.get(Chain, {
-      // where: { id: chainID }, // This is temporary until we got way to get the chain id and token id
-      where: { id: CHAIN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
-    });
-
-    if (!chainData) {
-      // CHAIN_DETAILS.id = chainID;
-      CHAIN_DETAILS.id = CHAIN_DETAILS.id;
-      chainData = new Chain(CHAIN_DETAILS);
-      await store.save(chainData);
-    }
-
-    // account data check and creation
-    if (!account) {
-      account = new Account({
-        id: who,
-        chainId: CHAIN_DETAILS.id,
-        balance: balanceValue,
-      });
-    } else {
-      account.balance = balanceValue;
-    }
-
-    await store.save(account);
 
     // account data saving
     balance = new Balance({
@@ -132,11 +133,10 @@ const handleAccountAndBalance = async (
       bondedBalance: 0n,
     });
 
-    await store.save(balance);
   } else {
     balance.freeBalance = balanceValue;
-    await store.save(balance);
   }
+  await store.save(balance);
 };
 
 
