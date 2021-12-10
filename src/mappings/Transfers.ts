d0afd3a5238b4417e8fec4874b240c4a24f216ea
NagatoPeinI1 <shalabhnegi123@gmail.com>
2021-12-10 11:05:45 +0530

    Changes made for bonded and unbonded balance

--- src/mappings/Transfers.ts
+++ src/mappings/Transfers.ts
@@ -18,51 +18,46 @@ export const handleTransfer = async ({
   const [from, to, value] = new Balances.TransferEvent(event).params;
   const tip = extrinsic?.tip || 0n
 
-  let transfer = await store.get(Transfer, {
-    where: { id: block.height.toString() },
+  await handleAccountAndBalance(
+    store,
+    block,
+    from.toString(),
+    value.toBigInt(),
+    true,
+    tip
+  );
+
+  await handleAccountAndBalance(
+    store,
+    block,
+    to.toString(),
+    value.toBigInt(),
+    false,
+    tip
+  );
+
+  // token data check and creation
+  let tokenData: Token | undefined = await store.get(Token, {
+    where: { id: TOKEN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
   });
 
-  if (!transfer) {
-    await handleAccountAndBalance(
-      store,
-      block,
-      from.toString(),
-      value.toBigInt(),
-      true,
-      tip
-    );
-    await handleAccountAndBalance(
-      store,
-      block,
-      to.toString(),
-      value.toBigInt(),
-      false,
-      tip
-    );
-
-    // token data check and creation
-    let tokenData: Token | undefined = await store.get(Token, {
-      where: { id: TOKEN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
-    });
-
-    if (!tokenData) {
-      tokenData = new Token(TOKEN_DETAILS);
-      await store.save(tokenData);
-    }
-
-    // account data creation and saving
-
-    transfer = new Transfer({
-      id: block.height.toString(),
-      senderAccount: from.toString(),
-      receiverAccount: to.toString(),
-      tokenId: tokenData.id,
-      amount: value.toBigInt(),
-      timestamp: timestampToDate(block),
-    });
-
-    await store.save(transfer);
+  if (!tokenData) {
+    tokenData = new Token(TOKEN_DETAILS);
+    await store.save(tokenData);
   }
+
+  // account data creation and saving
+
+  const transfer = new Transfer({
+    id: block.height.toString(),
+    senderAccount: from.toString(),
+    receiverAccount: to.toString(),
+    tokenId: tokenData.id,
+    amount: value.toBigInt(),
+    timestamp: timestampToDate(block),
+  });
+
+  await store.save(transfer);
 };
 
 const handleAccountAndBalance = async (
@@ -73,6 +68,7 @@ const handleAccountAndBalance = async (
   isFrom: boolean,
   tip: bigint
 ) => {
+
   let balance = await store.get(Balance, {
     where: { accountId: who },
   });
@@ -91,9 +87,7 @@ const handleAccountAndBalance = async (
   }
 
   // chain data check and creation
-  // const chainID = `${block.height}-${who}` || CHAIN_DETAILS.id;
   let chainData: Chain | undefined = await store.get(Chain, {
-    // where: { id: chainID }, // This is temporary until we got way to get the chain id and token id
     where: { id: CHAIN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
   });
 
@@ -107,7 +101,7 @@ const handleAccountAndBalance = async (
   let account = await store.get(Account, {
     where: { id: who },
   });
-  
+
   // account data check and creation
   if (!account) {
     account = new Account({
@@ -122,7 +116,6 @@ const handleAccountAndBalance = async (
   await store.save(account);
 
   if (!balance) {
-
     // account data saving
     balance = new Balance({
       id: block.height.toString(),
@@ -147,17 +140,69 @@ export const handleBondedBalance = async ({
   extrinsic,
 }: EventContext & StoreContext): Promise<void> => {
   const [accountId, bondedAmount] = new Balances.BondedEvent(event).params;
+  const who: string = accountId.toString();
+  const amount: bigint = bondedAmount.toBigInt() || 0n;
+
+  // token data check and creation
+  let tokenData: Token | undefined = await store.get(Token, {
+    where: { id: TOKEN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
+  });
+
+  if (!tokenData) {
+    tokenData = new Token(TOKEN_DETAILS);
+    await store.save(tokenData);
+  }
+
+  // chain data check and creation
+  let chainData: Chain | undefined = await store.get(Chain, {
+    where: { id: CHAIN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
+  });
+
+  if (!chainData) {
+    // CHAIN_DETAILS.id = chainID;
+    CHAIN_DETAILS.id = CHAIN_DETAILS.id;
+    chainData = new Chain(CHAIN_DETAILS);
+    await store.save(chainData);
+  }
+
+  let account = await store.get(Account, {
+    where: { id: who },
+  });
+
+  // account data check and creation
+  if (!account) {
+    account = new Account({
+      id: who,
+      chainId: CHAIN_DETAILS.id,
+      balance: amount,
+    });
+  } else {
+    account.balance = account.balance + amount;
+  }
+
+  await store.save(account);
+
 
   let balance: Balance | undefined = await store.get(Balance, {
     where: { accountId: accountId.toString() },
   });
 
-  if (balance) {
+  if (!balance) {
+    balance = new Balance({
+      id: block.height.toString(),
+      accountId: account.id,
+      tokenId: tokenData.id,
+      timestamp: timestampToDate(block),
+      freeBalance: amount,
+      bondedBalance: 0n,
+    });
+    
+  } else {
     let bondedBalance: bigint = balance.bondedBalance || 0n;
-    bondedBalance = bondedBalance + (bondedAmount.toBigInt() || 0n);
+    bondedBalance = bondedBalance + amount;
     balance.bondedBalance = bondedBalance;
-    await store.save(balance);
   }
+  await store.save(balance);
 }
 
 export const handleUnBondedBalance = async ({
@@ -167,16 +212,66 @@ export const handleUnBondedBalance = async ({
   extrinsic,
 }: EventContext & StoreContext): Promise<void> => {
   const [accountId, bondedAmount] = new Balances.UnBondedEvent(event).params;
+  const who: string = accountId.toString();
+  const amount: bigint = bondedAmount.toBigInt() || 0n;
+
+  // token data check and creation
+  let tokenData: Token | undefined = await store.get(Token, {
+    where: { id: TOKEN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
+  });
+
+  if (!tokenData) {
+    tokenData = new Token(TOKEN_DETAILS);
+    await store.save(tokenData);
+  }
+
+  // chain data check and creation
+  let chainData: Chain | undefined = await store.get(Chain, {
+    where: { id: CHAIN_DETAILS.id }, // This is temporary until we got way to get the chain id and token id
+  });
+
+  if (!chainData) {
+    CHAIN_DETAILS.id = CHAIN_DETAILS.id;
+    chainData = new Chain(CHAIN_DETAILS);
+    await store.save(chainData);
+  }
+  
+  let account = await store.get(Account, {
+    where: { id: who },
+  });
+
+  // account data check and creation
+  if (!account) {
+    account = new Account({
+      id: who,
+      chainId: CHAIN_DETAILS.id,
+      balance: amount,
+    });
+  } else {
+    account.balance = account.balance - amount;
+  }
+
+  await store.save(account);
 
 
   let balance: Balance | undefined = await store.get(Balance, {
     where: { accountId: accountId.toString() },
   });
 
-  if (balance) {
+  if (!balance) {
+    balance = new Balance({
+      id: block.height.toString(),
+      accountId: account.id,
+      tokenId: tokenData.id,
+      timestamp: timestampToDate(block),
+      freeBalance: amount,
+      bondedBalance: 0n,
+    });
+    
+  } else {
     let bondedBalance: bigint = balance.bondedBalance || 0n;
-    bondedBalance = bondedBalance - (bondedAmount.toBigInt() || 0n);
+    bondedBalance = bondedBalance - amount;
     balance.bondedBalance = bondedBalance;
-    await store.save(balance);
   }
+  await store.save(balance);
 }
--- testApi.js
+++ testApi.js
@@ -1,11 +1,14 @@
 const polk= require("@polkadot/api")
 async function m(){
  const api = await polk.ApiPromise.create({ provider: new polk.WsProvider('wss://kusama-rpc.polkadot.io/') })
- const hash = "0xe0a632e3342e4d795fb756bce5fc8f7a4c35f38d875cbf5c4d97f46f55dc7036"
- const apiAt = await api.at(hash)
-const paraId = 2008
- const queryData = await (await apiAt.query.registrar.paras(paraId)).toJSON()
+//  const hash = '0xfa0b79064d7d480c1e31513b8c1116ef9d7a026a253befc16ec0ffeba0465ce0'
+//  const apiAt = await api.at(hash)
+// const paraId = 2008
+//  const queryData = await (await apiAt.query.registrar.paras(paraId)).toJSON()
+// const queryData = await api.query.balances.account('5CDPbtMikAFCGYtDCX6NimnAFZyYPVULVAFJajEBSzj76zrn')
+const queryData = await api.query.balances.locks('5FA2jYeaoLNYBkGvNAJCwiFU2WfVDwgEm23NxH3Kv2qZuDxj')
+
  
- console.log(" endingPeriod ::::  ",queryData)
+ console.log(" endingPeriod ::::  ",queryData.toJSON())
 }
 m()
