6b017a3205e35adccd07a88c4c817e13a14ff376
Rikonorus <djavad199450@gmail.com>
2022-01-21 22:32:58 +0300

    1

--- src/mapping/Balances/reserveRepatriatedHandler.ts
+++ src/mapping/Balances/reserveRepatriatedHandler.ts
@@ -34,7 +34,7 @@ export const reserveRepatriatedHandler: EventHandler = async (
 };
 
 const getEvent = (ctx: EventHandlerContext): EventType => {
-  console.log("KRI;reserveRepatriatedHandler", JSON.stringify(ctx.event));
+  console.log("KRI;reserveRepatriatedHandler", JSON.stringify(ctx.event.name));
   const event = new BalancesReserveRepatriatedEvent(ctx);
   if (event.asV2008) {
     const [from, to, amount, destinationStatus] = event.asV2008;
