d408341c82a89e15e10f9d9d58d3c2dd5d733282
Rikonorus <djavad199450@gmail.com>
2022-02-22 09:20:07 +0300

    adjust BALANCES_RPC_BLOCK_TIMESTAMP_OFFSET

--- src/constants.ts
+++ src/constants.ts
@@ -51,7 +51,7 @@ export const PROCESSOR_BATCH_SIZE = 500;
 export const BALANCES_RPC_BLOCK_CHUNK_SIZE = 500;
 export const BALANCES_RPC_PER_BATCH = 10;
 export const BALANCES_RPC_BLOCK_HEIGHT_OFFSET = 300;
-export const BALANCES_RPC_BLOCK_TIMESTAMP_OFFSET = 24 * 60 * 60 * 1000; // 1 day in ms
+export const BALANCES_RPC_BLOCK_TIMESTAMP_OFFSET = 8 * 60 * 60 * 1000; // each 8 hours
 
 export enum RpcQueriesPath {
   systemAccountInfo = "query.system.account",
